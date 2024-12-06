8dc016e82290ece28e419171aa2140acfc5ae766
Dax <daxnavarrete@gmail.com>
2024-12-06 11:50:32 -0500

    Fix paths with os

--- src/utils/load_config.py
+++ src/utils/load_config.py
@@ -1,7 +1,13 @@
+import os
 import yaml
 
+def load_config():
+    config_path = os.path.join(os.path.dirname(__file__), '..', '..', 'config', 'config.yaml')
+    
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"Configuration file not found: {config_path}")
 
-def load_config(config_path="config/config.yaml"):
-    with open(config_path, "r") as file:
+    with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
+    
     return config
