8d540160090be1049d7eddc49dee6149d34b9d90
Dax <daxnavarrete@gmail.com>
2024-12-06 09:52:41 -0500

    refractor logging with a logger

--- src/utils/face_operations.py
+++ src/utils/face_operations.py
@@ -7,6 +7,7 @@ from .load_config import load_config
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 config = load_config()
 
@@ -24,14 +25,14 @@ def create_faces_encoding_file():
         try:
             image_cv2 = cv2.imread(image_path)
             if image_cv2 is None:
-                logging.warning(
+                logger.warning(
                     f"Image {image_path} not found or cannot be read.")
                 continue
 
             image_rgb = cv2.cvtColor(image_cv2, cv2.COLOR_BGR2RGB)
             encodings = face_recognition.face_encodings(image_rgb)
             if not encodings:
-                logging.warning(f"No face found in image {image_path}.")
+                logger.warning(f"No face found in image {image_path}.")
                 continue
 
             encoding = encodings[0]
@@ -39,10 +40,10 @@ def create_faces_encoding_file():
                 ENCODINGS_DIR, f"encoding{face_index}.pickle")
             with open(encoding_path, "wb") as encoding_file:
                 pickle.dump(encoding, encoding_file)
-            logging.info(f"Encoding for face {face_index} saved successfully.")
+            logger.info(f"Encoding for face {face_index} saved successfully.")
         except Exception as e:
-            logging.error(f"An error occurred while processing {
-                          image_path}: {e}")
+            logger.error(f"An error occurred while processing {
+                         image_path}: {e}")
 
 
 def read_faces_encoding_file():
@@ -57,11 +58,10 @@ def read_faces_encoding_file():
             with open(encoding_path, "rb") as encoding_file:
                 encoding = pickle.load(encoding_file)
                 encodings.append(encoding)
-            logging.info(f"Encoding for face {
-                         face_index} loaded successfully.")
+            logger.info(f"Encoding for face {face_index} loaded successfully.")
         except FileNotFoundError:
-            logging.warning(f"Encoding file {encoding_path} not found.")
+            logger.warning(f"Encoding file {encoding_path} not found.")
         except Exception as e:
-            logging.error(f"An error occurred while reading {
-                          encoding_path}: {e}")
+            logger.error(f"An error occurred while reading {
+                         encoding_path}: {e}")
     return encodings
